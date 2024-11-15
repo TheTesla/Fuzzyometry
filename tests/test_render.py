be56a60feec7b66fa5d50c9f050923730bc10b1f
Stefan Helmert <stefan.helmert@t-online.de>
2024-11-15 16:58:54 +0100

    first test; CI/CD

---
+++ tests/test_render.py
@@ -0,0 +1,23 @@
+import subprocess
+
+import pytest
+from numba import njit
+
+from fuzzyometry.bodies import fz_sphere
+
+@njit
+def sphere(x, y, z):
+    return fz_sphere((x,y,z), 20)
+
+def is_around(val1, val2, tol=0.001):
+    return abs(val1-val2) < tol
+
+def test_fz_sphere():
+    assert is_around(fz_sphere((0,0,0),3), -3)
+    assert is_around(fz_sphere((1,0,0),3), -2)
+    assert is_around(fz_sphere((0,1,0),3), -2)
+    assert is_around(fz_sphere((0,0,1),3), -2)
+    assert is_around(fz_sphere((1,1,0),3), -1.586)
+    assert is_around(fz_sphere((1,0,1),3), -1.586)
+    assert is_around(fz_sphere((0,1,1),3), -1.586)
+    assert is_around(sphere(1,0,0), -19)
