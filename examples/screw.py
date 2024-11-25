9d28e415376ef504b9ad2be663eb757f492b08b3
Stefan Helmert <stefan.helmert@t-online.de>
2024-11-25 20:30:28 +0100

    multi-start thread; thread stepsize 1

--- examples/screw.py
+++ examples/screw.py
@@ -3,6 +3,8 @@
 from numba import njit
 from xyzcad import render
 
+from fuzzyometry import bodies as fzbdy
+from fuzzyometry import combinations as cmb
 from fuzzyometry import threads as fzthrd
 
 
@@ -10,11 +12,12 @@ from fuzzyometry import threads as fzthrd
 def f(x, y, z):
     if max(abs(x), abs(y), abs(z)) > 20:
         return False
-
-    if fzthrd.fz_thread((x, y, z), 10) > 0:
+    a = fzbdy.fz_cuboid((x, y, z), (25, 25, 30), 0.2)
+    b = fzthrd.fz_thread((x, y, z / 6), 10, 4, 1.0)
+    if cmb.fz_and_chamfer(1.5, a, b) > 0:
         return False
 
     return True
 
 
-render.renderAndSave(f, "screw.stl", 0.2)
+render.renderAndSave(f, "screw.stl", 0.1)
