05dd4d232acd0ff239ccb36cfea3b0c24995db83
Stefan Helmert <stefan.helmert@t-online.de>
2024-11-30 18:05:01 +0100

    paameterized threads, fuzzy thread profile correct

--- examples/screw.py
+++ examples/screw.py
@@ -12,9 +12,12 @@ from fuzzyometry import threads as fzthrd
 def f(x, y, z):
     if max(abs(x), abs(y), abs(z)) > 20:
         return False
-    a = fzbdy.fz_cuboid((x, y, z), (25, 25, 30), 0.2)
-    b = fzthrd.fz_thread((x, y, z / 6), 10, 4, 1.0)
-    if cmb.fz_and_chamfer(1.5, a, b) > 0:
+    k = 2
+    s = 2
+    a = 0.2*fzbdy.fz_cuboid((x, y, z), (25, 25, 30), 0.2)
+    b = fzthrd.fz_thread((x, y, z / 4), 5, 4, s, 1.0)
+    c = fzthrd.fz_thread((x, y, 1./2.), 1.0, 4, k, 1.0)
+    if cmb.fz_and_chamfer(1., a, b, -c) > 0:
         return False
 
     return True
