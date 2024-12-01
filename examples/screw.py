c2680b592235d90cb63cc38e8b2ca91d55ce7c2b
Stefan Helmert <stefan.helmert@t-online.de>
2024-12-01 22:26:30 +0100

    SpiroBrixx compatible screw example

--- examples/screw.py
+++ examples/screw.py
@@ -10,14 +10,13 @@ from fuzzyometry import threads as fzthrd
 
 @njit
 def f(x, y, z):
-    if max(abs(x), abs(y), abs(z)) > 20:
-        return False
-    k = 2
-    s = 2
-    a = 0.2*fzbdy.fz_cuboid((x, y, z), (25, 25, 30), 0.2)
-    b = fzthrd.fz_thread((x, y, z / 4), 5, 4, s, 1.0)
-    c = fzthrd.fz_thread((x, y, 1./2.), 1.0, 4, k, 1.0)
-    if cmb.fz_and_chamfer(1., a, b, -c) > 0:
+    k = 0.9
+    s = 1.6
+    a = fzbdy.fz_cuboid((x, y, z), (25, 25, 60), 0.2)
+    #b = fzthrd.fz_thread((x, y, 1.01 * z / 6), 7.5, 4, s, 1.0)
+    b = fzthrd.fz_thread((x, y, 1.02 * z / 6), 7.5, 4, s, 1.0)
+    c = fzthrd.fz_thread((x, y, 1./2.), 3.6, 4, k, 1.0)
+    if cmb.fz_and_chamfer(0.5, a, b, -c) > 0:
         return False
 
     return True
